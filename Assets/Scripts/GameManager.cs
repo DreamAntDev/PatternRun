e35a337a10b0d37c3dad8ae77baf5ce3cf393a3a
Nammu <mod157@naver.com>
2021-07-01 22:01:07 +0900

    Line Renderer 추가

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -16,6 +16,7 @@ public class GameManager : MonoBehaviour
     private void Awake()
     {
         instance = this;
+
         DontDestroyOnLoad(this);
         chainText = string.Empty;
     }
---
+++ Assets/Scripts/LineTest.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LineTest : MonoBehaviour
+{
+    [SerializeField] private Transform[] points;
+    [SerializeField] private LineController line;
+    void Start()
+    {
+
+        line.SetUpLine(points);
+    }
+
+}
