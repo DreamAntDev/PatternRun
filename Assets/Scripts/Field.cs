f13c1736ecd8602cacd3630d3699c5b407d51701
KwakDongKyu <dongkyu0608@gmail.com>
2021-09-20 17:44:16 +0900

    맵 프리팹 정리

--- Assets/Scripts/Field.cs
+++ Assets/Scripts/Field.cs
@@ -5,6 +5,14 @@ using UnityEngine;
 public class Field : MonoBehaviour
 {
     public SpriteRenderer sprite;
+    public BoxCollider2D groundCollider;
+
+    private void Awake()
+    {
+        this.groundCollider.size = new Vector3(sprite.bounds.size.x/this.gameObject.transform.localScale.x, this.groundCollider.size.y);
+        float height = (float)sprite.bounds.size.y / (float)this.gameObject.transform.localScale.y;
+        this.groundCollider.transform.localPosition = new Vector3(0,  -height/ 2.0f + this.groundCollider.size.y/2.0f);
+    }
     //private void OnBecameInvisible()
     //{
     //    FieldManager.Instance.OnInvisibleField(this.gameObject);
